74bec5ac0176aebbaadfed5597f17be357432527
DESKTOP-NTHHVLJ\Administrator <fengzilijie@qq.com>
2021-08-11 11:32:16 +0800

    debug on windows

---
+++ using-dylib/lib/build.bat
@@ -0,0 +1,25 @@
+rm -rf build
+mkdir build
+cd build
+
+@REM cd build && cmake .. && make && cd ..
+
+if exist VSProject2019 (
+    del /q VSProject2019
+    for /D %%i in ("VSProject2019/**") DO (
+    	if not %%i==".vs" (
+            if exist "VSProject2019/%%i" (
+                echo Del folder "%%i"
+                rd /S /Q "VSProject2019/%%i"
+            )
+    	)
+    )
+)
+
+cmake -B "VSProject2019" "sdk/core" ..
+cmake --build "VSProject2019" --target "stringutil" --config=debug --parallel || echo "Compile Failed!" && exit /b
+
+cd ..
+rm -rf ..\bin\Debug/\netcoreapp3.1\stringutil.dll
+mv build\VSProject2019\Debug\stringutil.dll ..\bin\Debug\netcoreapp3.1
+mv build\VSProject2019\Debug\stringutil.pdb ..\bin\Debug\netcoreapp3.1
--- using-dylib/lib/stringutil.h
+++ using-dylib/lib/stringutil.h
@@ -1,7 +1,22 @@
 
 #include <stdio.h>
 
+#ifdef _WIN32
+#define API_DEC __declspec(dllexport)
+#else
+#if __GNUC__ >= 4
+#define API_DEC __attribute__((visibility("default")))
+#else
+#define API_DEC
+#endif
+#endif
+
+#ifdef __cplusplus
 extern "C"
+#endif
 {
-  const char *GetLog(void);
+  API_DEC const char *GetLog(void);
+
+#ifdef __cplusplus
 }
+#endif
